77176da06716c34862dec8388a433d556e7a3544
Alex <alexrockycahyadi@gmail.com>
2022-06-26 14:44:12 +0700

    init spring

---
+++ toko-online-backend/src/main/java/com/tokoonlinebackendservice/TokoOnlineBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tokoonlinebackendservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokoOnlineBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokoOnlineBackendApplication.class, args);
+	}
+
+}
---
+++ toko-online-backend/src/test/java/com/tokoonlinebackendservice/TokoOnlineBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tokoonlinebackendservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokoOnlineBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
